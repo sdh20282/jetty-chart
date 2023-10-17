02167a9192cdf5f82b713f9a812e3db2934f56ff
sdh20282 <sdh20282@gmail.com>
2023-10-17 17:46:36 +0900

    Feat : bar 차트 background-line, 범주 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,66 @@
-import { VerticalBar } from "./jetty-chart/src";
+import { NormalBar } from "./jetty-chart/src";
 
 const data = [
   {
-    value: 10,
-    label: "test1",
+    value: Math.floor(Math.random() * 1000),
+    label: "test1"
   },
   {
-    value: 10,
-    label: "test1",
+    value: Math.floor(Math.random() * 1000),
+    label: "test2"
   },
   {
-    value: 10,
-    label: "test1",
+    value: Math.floor(Math.random() * 1000),
+    label: "test3"
   },
   {
-    value: 10,
-    label: "test1",
+    value: Math.floor(Math.random() * 1000),
+    label: "test4"
   },
   {
-    value: 10,
-    label: "test1",
-  },
+    value: Math.floor(Math.random() * 1000),
+    label: "test5"
+  }
 ];
 
-const MyVerticalBar = ({ data }) => {
-  return <VerticalBar data={data} width="400" height="300" backgroundColor="#c4c4c4" />;
+const myNormalBar = ({ data }) => {
+  return (
+    <NormalBar
+      data={data}
+      generalSettings={{
+        width: "500",
+        height: "300",
+        backgroundColor: "#fff",
+        padding: { top: "30", bottom: "50", left: "60", right: "130" }
+      }}
+      categorySettings={{
+        lineVisible: true,
+        lineOpacity: "1",
+        lineColor: "#c4c4c4",
+        lineWidth: "1",
+        categoryLocation: "10",
+        categorySize: "11",
+        categoryWeight: "500",
+        categoryColor: "#777",
+        categoryGap: "3",
+        categoryLineVisible: true,
+        categoryLineWidth: "2",
+        categoryLineColor: "#aaa"
+      }}
+      barSettings={{
+        barGap: "0.1"
+      }}
+    />
+  );
 };
 
 function App() {
   return (
     <div>
       hello world
-      {MyVerticalBar({ data })}
+      {myNormalBar({ data })}
     </div>
   );
 }
+
 export default App;
