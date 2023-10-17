89566e7fdcb91d4b07f6d371f8b1ece90f8406de
J3SUNG <wptjd6141@naver.com>
2023-10-17 17:47:42 +0900

    Feat: Pie 차트 기능 구현
    기본 파이 차트 생성
    차트 계산 생성
    props 데이터 처리
    디폴트 값 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import { VerticalBar } from "./jetty-chart/src";
+import { Pie } from "./jetty-chart/src/pies/pie/Pie";
 
 const data = [
   {
-    value: 10,
+    value: 0.25,
     label: "test1",
   },
   {
-    value: 10,
-    label: "test1",
+    value: 0.1,
+    label: "test2",
   },
   {
-    value: 10,
-    label: "test1",
+    value: 0.15,
+    label: "test3",
   },
   {
-    value: 10,
-    label: "test1",
+    value: 0.3,
+    label: "test4",
   },
   {
-    value: 10,
-    label: "test1",
+    value: 0.2,
+    label: "test5",
   },
 ];
 
@@ -27,11 +28,16 @@ const MyVerticalBar = ({ data }) => {
   return <VerticalBar data={data} width="400" height="300" backgroundColor="#c4c4c4" />;
 };
 
+const MyPie = ({ data }) => {
+  return <Pie data={data} width="400" height="300" backgroundColor="#c4c4c4" />;
+};
+
 function App() {
   return (
     <div>
       hello world
       {MyVerticalBar({ data })}
+      {MyPie({ data })}
     </div>
   );
 }
--- src/jetty-chart/src/pies/pie/Pie.js
+++ src/jetty-chart/src/pies/pie/Pie.js
@@ -1,5 +1,87 @@
-const Pie = () => {
-  return <div>Pie</div>;
+const Pie = ({
+  data,
+  generalSettings = {
+    width: "500",
+    height: "300",
+    backgroundColor: "#aaa",
+    padding: { top: "30", bottom: "50", left: "60", right: "130" },
+  },
+  pieSettings = {
+    color: ["red", "blue", "green", "yellow", "orange", "purple"],
+  },
+}) => {
+  const getCategoryDataPath = (
+    { value, label },
+    { startX, startY, endX, endY, isLargeArcFlag },
+    index
+  ) => {
+    const targetRad = 2 * Math.PI * value;
+    const targetRestRad = 2 * Math.PI * (1 - value);
+    const animationDuration = 0.2;
+    return (
+      <path
+        d={`M ${startX} ${startY} A 1 1 0 ${isLargeArcFlag} 1 ${endX} ${endY} L 0 0`}
+        fill="none"
+        stroke={pieSettings.color[index]}
+        strokeWidth="0.4"
+        strokeDasharray={`${targetRad} ${targetRestRad}`}
+        strokeDashoffset="0.025"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          begin={`${animationDuration * index}`}
+          from={`${targetRad}`}
+          to="0.025"
+          dur={`${animationDuration}`}
+          fill="freeze"
+        />
+      </path>
+    );
+  };
+
+  const getCoordinatesForPercent = (percent) => {
+    const x = Math.cos(2 * Math.PI * percent);
+    const y = Math.sin(2 * Math.PI * percent);
+
+    return [x, y];
+  };
+
+  const getPieChartPaths = (data) => {
+    let accumulatedPercent = 0;
+
+    return data.map(({ value, label }, index) => {
+      const [startX, startY] = getCoordinatesForPercent(accumulatedPercent);
+      accumulatedPercent += value;
+      const [endX, endY] = getCoordinatesForPercent(accumulatedPercent);
+      const isLargeArcFlag = value > 0.5 ? "1" : "0";
+
+      return getCategoryDataPath(
+        { value, label },
+        { startX, startY, endX, endY, isLargeArcFlag },
+        index
+      );
+    });
+  };
+
+  return (
+    <div>
+      <p>Pie</p>
+      <div>
+        <svg
+          id="pie"
+          width={generalSettings.width}
+          height={generalSettings.height}
+          style={{
+            backgroundColor: generalSettings.backgroundColor,
+            padding: `${generalSettings.padding.top}px ${generalSettings.padding.right}px ${generalSettings.padding.bottom}px ${generalSettings.padding.left}px`,
+          }}
+          viewBox="-1.5 -1.5 3 3"
+        >
+          {getPieChartPaths(data)}
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export { Pie };
