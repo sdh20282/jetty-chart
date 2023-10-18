7add516c79dbf8b0ad2fd5a1df65eb2e1fe9353c
sdh20282 <sdh20282@gmail.com>
2023-10-18 18:00:10 +0900

    Feat : bar 차트 에러 처리 추가

--- src/jetty-chart/src/bars/normal-bar/nomal-bar.js
+++ src/jetty-chart/src/bars/normal-bar/nomal-bar.js
@@ -1,53 +1,19 @@
 import { BarCommon } from "../bar-common/bar-common";
 
 import { getLevelAutoScope, getLevelCalculatedScope } from "../../common/utils/level/calculate-level";
-import { checkPadding, checkBarBorderRadius } from "../../common/utils/check-exception";
+import { checkBarBorderRadius } from "../../common/utils/exception/check-common-exception";
+import { checkNormalBar } from "../../common/utils/exception/check-bar-exception";
 
-const NormalBar = ({
-  data,
-  generalSettings: { width = 500, height = 300, backgroundColor = "#ffffff", padding = { top: 30, bottom: 50, left: 80, right: 130 } },
-  levelSettings: {
-    lineVisible = true,
-    lineOpacity = 1,
-    lineColor = "#c4c4c4",
-    lineWidth = 1,
-    levelAutoScope = true,
-    levelMaxScope = 100,
-    levelMinScope = 0,
-    levelTextGap = 10,
-    levelTextSize = 11,
-    levelTextWeight = 400,
-    levelTextColor = "#777",
-    levelTextMargin = 3,
-    levelLineVisible = true,
-    levelLineOpacity = 1,
-    levelLineColor = "#aaa",
-    levelLineWidth = 2,
-    showTopLevel = true
-  },
-  barSettings: {
-    chartPadding = 5,
-    barColor = "#8EA3BC",
-    barGap = 0.2,
-    barOnlyUpperRadus = true,
-    barBorderRadius = 0,
-    categoryTextGap = 14,
-    categoryTextSize = 11,
-    categoryTextWeight = 500,
-    categoryTextColor = "#777",
-    categoryTextMargin = 8,
-    categoryLineVisible = true,
-    categoryLineOpacity = 1,
-    categoryLineColor = "#aaa",
-    categoryLineWidth = 2
-  }
-}) => {
-  padding = checkPadding({ padding });
+const NormalBar = ({ data, generalSettings, levelSettings, barSettings }) => {
+  const result = checkNormalBar({ generalSettings, levelSettings, barSettings });
+  const { width, height, padding } = result.generalSettings;
+  const { levelAutoScope, levelMaxScope, levelMinScope } = result.levelSettings;
+  let { barGap, barBorderRadius, chartPadding, barColor, barOnlyUpperRadus } = result.barSettings;
 
   const levelResult = levelAutoScope ? getLevelAutoScope({ data }) : getLevelCalculatedScope({ maxScope: levelMaxScope, minScope: levelMinScope });
 
-  const chartAreaWidth = parseInt(width - padding.left - padding.right - chartPadding - chartPadding, 10);
-  const chartAreaHeight = parseInt(height - padding.bottom - padding.top, 10);
+  const chartAreaWidth = width - padding.left - padding.right - chartPadding - chartPadding;
+  const chartAreaHeight = height - padding.bottom - padding.top;
   const halfAreaWidth = chartAreaWidth / data.length / 2;
   const halfWidth = halfAreaWidth - barGap * halfAreaWidth;
   const lineGap = chartAreaHeight / (levelResult.level.length - 1);
@@ -66,46 +32,19 @@ const NormalBar = ({
   return (
     <BarCommon
       data={data}
-      generalSettings={{
-        width,
-        height,
-        backgroundColor,
-        padding: { top: padding.top, bottom: padding.bottom, left: padding.left, right: padding.right }
-      }}
+      generalSettings={{ ...result.generalSettings }}
       levelSettings={{
         level: levelResult.level,
-        lineVisible,
-        lineOpacity,
-        lineColor,
-        lineWidth,
-        levelTextGap,
-        levelTextSize,
-        levelTextWeight,
-        levelTextColor,
-        levelTextMargin,
-        levelLineVisible,
-        levelLineOpacity,
-        levelLineColor,
-        levelLineWidth,
-        showTopLevel
+        ...result.levelSettings
       }}
       categorySettings={{
         categoryPadding: chartPadding,
-        categoryTextGap,
-        categoryTextSize,
-        categoryTextWeight,
-        categoryTextColor,
-        categoryTextMargin,
-        categoryLineVisible,
-        categoryLineOpacity,
-        categoryLineColor,
-        categoryLineWidth
+        ...result.barSettings
       }}
     >
       <g transform={`translate(${chartPadding})`}>
         {data.map((d, idx) => {
           const x = (chartAreaWidth / data.length) * idx + chartAreaWidth / data.length / 2;
-          // const y = lineGap * (zeroIndex - idx);
           const height = (Math.abs(d.value) / (levelResult.maxScope - levelResult.minScope)) * chartAreaHeight;
           const realHeight = height >= barBorderRadius ? height - barBorderRadius : 0;
 
