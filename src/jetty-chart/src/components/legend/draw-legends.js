43689b6dc7d4cb5f5a169d3cb98a755c81571510
sdh20282 <sdh20282@gmail.com>
2023-10-23 17:52:27 +0900

    Feat : 설명 사용 여부 추가

--- src/jetty-chart/src/components/legend/draw-legends.js
+++ src/jetty-chart/src/components/legend/draw-legends.js
@@ -2,6 +2,7 @@ export const DrawLegends = ({
   keys,
   normalSettings: { colorPalette },
   legendSettings: {
+    useLegend,
     xLocation,
     yLocation,
     directionColumn,
@@ -17,35 +18,37 @@ export const DrawLegends = ({
   }
 }) => {
   return (
-    <g transform={`translate(${xLocation},${yLocation})`}>
-      {keys.map((k, idx) => {
-        return (
-          <g
-            key={k + String(idx)}
-            transform={directionColumn ? `translate(0,${(symbolSize + itemMargin) * idx})` : `translate(${(itemWidth + itemMargin) * idx})`}
-            width={itemWidth - symbolSize - symbolMargin}
-          >
-            <rect
-              width={symbolSize}
-              height={symbolSize}
-              fill={colorPalette[idx % colorPalette.length]}
-              rx={symbolRadius}
-              ry={symbolRadius}
-              transform="translate(0,0)"
-            ></rect>
-            <text
-              fontSize={legendSize}
-              fontWeight={legendWeight}
-              fill={legendColor}
-              dominantBaseline="middle"
-              textAnchor={legendOnStart ? "start" : "end"}
-              transform={`translate(${legendOnStart ? symbolSize + symbolMargin : itemWidth},${symbolSize / 2})`}
+    useLegend && (
+      <g transform={`translate(${xLocation},${yLocation})`}>
+        {keys.map((k, idx) => {
+          return (
+            <g
+              key={k + String(idx)}
+              transform={directionColumn ? `translate(0,${(symbolSize + itemMargin) * idx})` : `translate(${(itemWidth + itemMargin) * idx})`}
+              width={itemWidth - symbolSize - symbolMargin}
             >
-              {k}
-            </text>
-          </g>
-        );
-      })}
-    </g>
+              <rect
+                width={symbolSize}
+                height={symbolSize}
+                fill={colorPalette[idx % colorPalette.length]}
+                rx={symbolRadius}
+                ry={symbolRadius}
+                transform="translate(0,0)"
+              ></rect>
+              <text
+                fontSize={legendSize}
+                fontWeight={legendWeight}
+                fill={legendColor}
+                dominantBaseline="middle"
+                textAnchor={legendOnStart ? "start" : "end"}
+                transform={`translate(${legendOnStart ? symbolSize + symbolMargin : itemWidth},${symbolSize / 2})`}
+              >
+                {k}
+              </text>
+            </g>
+          );
+        })}
+      </g>
+    )
   );
 };
