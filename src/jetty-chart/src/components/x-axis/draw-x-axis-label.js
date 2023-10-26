54d784f2689fa0a7bf903d37b179de80153c3c2b
sdh20282 <sdh20282@gmail.com>
2023-10-26 17:01:50 +0900

    Refactor : component return 방식 변경

--- src/jetty-chart/src/components/x-axis/draw-x-axis-label.js
+++ src/jetty-chart/src/components/x-axis/draw-x-axis-label.js
@@ -18,6 +18,10 @@ export const DrawXAxisLabel = ({
     sideLineWidth
   }
 }) => {
+  if (!useLabel) {
+    return;
+  }
+
   const totalLabelMargin = labelMargin + sideLineSize;
   const labelLocation = height + totalLabelMargin;
 
@@ -25,63 +29,61 @@ export const DrawXAxisLabel = ({
   xAxisInitialPosition ??= 0;
 
   return (
-    useLabel && (
-      <g
-        transform={
-          horizontal
-            ? `translate(${labelOnBottom ? -totalLabelMargin : labelLocation},${padding})`
-            : `translate(${padding},${labelOnBottom ? labelLocation : -totalLabelMargin})`
-        }
-      >
-        {xAxis.map((d, idx) => {
-          const x = xAxisWidth * idx + xAxisInitialPosition;
+    <g
+      transform={
+        horizontal
+          ? `translate(${labelOnBottom ? -totalLabelMargin : labelLocation},${padding})`
+          : `translate(${padding},${labelOnBottom ? labelLocation : -totalLabelMargin})`
+      }
+    >
+      {xAxis.map((d, idx) => {
+        const x = xAxisWidth * idx + xAxisInitialPosition;
 
-          return (
-            <g key={"category-" + d + "-" + idx} transform={horizontal ? `translate(0, ${x})` : `translate(${x})`}>
-              {sideLineVisible && (
-                <line
-                  x1={horizontal ? (labelOnBottom ? totalLabelMargin - sideLineSize : -totalLabelMargin + sideLineSize) : "0"}
-                  x2={horizontal ? (labelOnBottom ? totalLabelMargin : -totalLabelMargin) : "0"}
-                  y1={horizontal ? "0" : labelOnBottom ? -totalLabelMargin + sideLineSize : totalLabelMargin - sideLineSize}
-                  y2={horizontal ? "0" : labelOnBottom ? -totalLabelMargin : totalLabelMargin}
-                  stroke={sideLineColor}
-                  strokeOpacity={sideLineOpacity}
-                  strokeWidth={sideLineWidth}
-                ></line>
-              )}
+        return (
+          <g key={"category-" + d + "-" + idx} transform={horizontal ? `translate(0, ${x})` : `translate(${x})`}>
+            {sideLineVisible && (
+              <line
+                x1={horizontal ? (labelOnBottom ? totalLabelMargin - sideLineSize : -totalLabelMargin + sideLineSize) : "0"}
+                x2={horizontal ? (labelOnBottom ? totalLabelMargin : -totalLabelMargin) : "0"}
+                y1={horizontal ? "0" : labelOnBottom ? -totalLabelMargin + sideLineSize : totalLabelMargin - sideLineSize}
+                y2={horizontal ? "0" : labelOnBottom ? -totalLabelMargin : totalLabelMargin}
+                stroke={sideLineColor}
+                strokeOpacity={sideLineOpacity}
+                strokeWidth={sideLineWidth}
+              ></line>
+            )}
 
-              <g transform={`translate(${horizontal ? 0 : labelMove},${horizontal ? -labelMove : 0}) rotate(${labelRotate})`}>
-                <text
-                  dominantBaseline={horizontal ? "hanging" : labelOnBottom ? "hanging" : "ideographic"}
-                  textAnchor={
-                    horizontal
-                      ? labelOnBottom
-                        ? "end"
-                        : "start"
-                      : labelRotate === 0
-                      ? "middle"
-                      : labelRotate < 0
-                      ? labelOnBottom
-                        ? "end"
-                        : "start"
-                      : labelOnBottom
-                      ? "start"
-                      : "end"
-                  }
-                  fontSize={labelSize}
-                  fontWeight={labelWeight}
-                  fill={labelColor}
-                  opacity={labelOpacity}
-                  transform={`translate(0,-${horizontal ? labelSize / 2 : 0})`}
-                >
-                  {d}
-                </text>
-              </g>
+            <g transform={`translate(${horizontal ? 0 : labelMove},${horizontal ? -labelMove : 0}) rotate(${labelRotate})`}>
+              <text
+                dominantBaseline={horizontal ? "hanging" : labelOnBottom ? "hanging" : "ideographic"}
+                textAnchor={
+                  horizontal
+                    ? labelOnBottom
+                      ? "end"
+                      : "start"
+                    : labelRotate === 0
+                    ? "middle"
+                    : labelRotate < 0
+                    ? labelOnBottom
+                      ? "end"
+                      : "start"
+                    : labelOnBottom
+                    ? "start"
+                    : "end"
+                }
+                fontSize={labelSize}
+                fontWeight={labelWeight}
+                fill={labelColor}
+                opacity={labelOpacity}
+                transform={`translate(0,-${horizontal ? labelSize / 2 : 0})`}
+              >
+                {d}
+              </text>
             </g>
-          );
-        })}
-      </g>
-    )
+          </g>
+        );
+      })}
+    </g>
   );
 };
 /* eslint-enable complexity */
--- src/jetty-chart/src/components/y-axis/draw-y-axis-legend.js
+++ src/jetty-chart/src/components/y-axis/draw-y-axis-legend.js
@@ -3,30 +3,31 @@ export const DrawYAxisLegend = ({
   normalSettings: { totalWidth, totalHeight, horizontal },
   legendSettings: { useLegend, legendOnLeft, legendMargin, legendSize, legendWeight, legendOpacity, legendColor, legendReverse, legendMove }
 }) => {
+  if (!yLegend || !useLegend) {
+    return;
+  }
+
   const height = totalHeight / 2 + (horizontal ? legendMove : -legendMove);
 
   return (
-    yLegend &&
-    useLegend && (
-      <g
-        transform={
-          horizontal
-            ? `translate(${height},${legendOnLeft ? -legendMargin : totalWidth + legendMargin})`
-            : `translate(${legendOnLeft ? -legendMargin : totalWidth + legendMargin}, ${height})`
-        }
+    <g
+      transform={
+        horizontal
+          ? `translate(${height},${legendOnLeft ? -legendMargin : totalWidth + legendMargin})`
+          : `translate(${legendOnLeft ? -legendMargin : totalWidth + legendMargin}, ${height})`
+      }
+    >
+      <text
+        fontSize={legendSize}
+        fontWeight={legendWeight}
+        fill={legendColor}
+        opacity={legendOpacity}
+        dominantBaseline="middle"
+        textAnchor="middle"
+        transform={horizontal ? `rotate(${legendReverse ? 180 : 0})` : `rotate(${legendReverse ? 90 : -90})`}
       >
-        <text
-          fontSize={legendSize}
-          fontWeight={legendWeight}
-          fill={legendColor}
-          opacity={legendOpacity}
-          dominantBaseline="middle"
-          textAnchor="middle"
-          transform={horizontal ? `rotate(${legendReverse ? 180 : 0})` : `rotate(${legendReverse ? 90 : -90})`}
-        >
-          {yLegend}
-        </text>
-      </g>
-    )
+        {yLegend}
+      </text>
+    </g>
   );
 };
