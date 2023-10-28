338cfa028efab2be30e59b8b9b0a626b02d57e68
sdh20282 <sdh20282@gmail.com>
2023-10-29 03:54:18 +0900

    Feat : dashed animation 추가 - grid line 생성 시 line tag -> path tag 변경

--- src/jetty-chart/src/components/x-axis/draw-x-axis-grid-line.js
+++ src/jetty-chart/src/components/x-axis/draw-x-axis-grid-line.js
@@ -1,3 +1,167 @@
+// import { useRef } from "react";
+
+// import styles from "./x-axis-grid-line.module.css";
+
+// /* eslint-disable complexity */
+// export const DrawXAxisGridLine = ({
+//   normalSettings: { xAxis, horizontal, width, height, padding, xAxisInitialPosition, xAxisWidth },
+//   lineSettings: { lineVisible, lineOpacity, lineColor, lineWidth, lineDash, lineDashWidth, lineDashGap, lineRound, showEndLine },
+//   animationSettings: {
+//     useAnimation,
+//     appearType,
+//     appearDuration,
+//     appearStartDelay,
+//     appearItemDelay,
+//     appearTimingFunction,
+//     appearStartFrom,
+//     moveLine,
+//     moveDuration,
+//     moveStartDelay,
+//     moveItemDelay,
+//     moveTimingFunction
+//   }
+// }) => {
+//   const prevXAxis = useRef({});
+//   const prevXAxisTemp = useRef({});
+
+//   if (!lineVisible) {
+//     return;
+//   }
+
+//   const animationXAxisStart = appearStartFrom.split("-")[0];
+//   const animationYAxisStart = appearStartFrom.split("-")[1];
+//   const prevXAxisKeys = Object.keys(prevXAxis.current);
+//   const ms = new Date().valueOf();
+
+//   padding ??= 0;
+//   xAxisInitialPosition ??= 0;
+
+//   if (moveLine) {
+//     prevXAxis.current = { ...prevXAxisTemp.current };
+//     prevXAxisTemp.current = [];
+//   }
+
+//   return (
+//     <g>
+//       {showEndLine && (
+//         <g transform={horizontal ? `translate(0,0)` : `translate(0,0)`}>
+//           <line
+//             key={"background-line-x-" + 0 + "-" + 0}
+//             x1={horizontal ? "0" : 0}
+//             x2={horizontal ? height : 0}
+//             y1={horizontal ? 0 : "0"}
+//             y2={horizontal ? 0 : height}
+//             stroke={lineColor}
+//             strokeOpacity={lineOpacity}
+//             strokeWidth={lineWidth}
+//             strokeDasharray={lineDash ? `${lineDashWidth},${lineDashGap}` : 0}
+//             strokeLinecap={lineRound ? "round" : ""}
+//             className={useAnimation ? (appearType === "draw" ? styles.drawLine : appearType === "fade" ? styles.fadeLine : "") : ""}
+//             style={{
+//               "--line-width": `${height}px`,
+//               "--line-offset": `${
+//                 (!horizontal && animationYAxisStart === "bottom") || (horizontal && animationYAxisStart !== "bottom") ? -height : height
+//               }px`,
+//               "--animation-duration": `${appearDuration}s`,
+//               "--animation-timing-function": "ease",
+//               "--animation-delay": `${appearStartDelay + appearItemDelay * (animationXAxisStart === "left" ? 0 : xAxis.length + 1)}s`
+//             }}
+//           ></line>
+//         </g>
+//       )}
+//       {
+//         <g transform={horizontal ? `translate(0,${padding})` : `translate(${padding},0)`}>
+//           {xAxis.map((d, idx) => {
+//             const x = xAxisWidth * idx + xAxisInitialPosition;
+
+//             // 현재 위치 정보 저장
+//             prevXAxisTemp.current[d] = x;
+
+//             // 라인 리렌더링을 안할 경우
+//             let useMove = false;
+//             let move = 0;
+
+//             if (moveLine) {
+//               // 이전 위치에 현재 위치가 포함되는지 확인
+//               if (prevXAxisKeys.includes(String(d))) {
+//                 move = x - prevXAxis.current[d];
+//                 useMove = true;
+//               }
+//             }
+
+//             return (
+//               <line
+//                 key={"background-line-x-" + ms + "-" + d}
+//                 x1={horizontal ? "0" : x - move}
+//                 x2={horizontal ? height : x - move}
+//                 y1={horizontal ? x - move : "0"}
+//                 y2={horizontal ? x - move : height}
+//                 stroke={lineColor}
+//                 strokeOpacity={lineOpacity}
+//                 strokeWidth={lineWidth}
+//                 strokeDasharray={lineDash && d !== 0 ? `${lineDashWidth},${lineDashGap}` : "0"}
+//                 strokeLinecap={lineRound ? "round" : ""}
+//                 className={
+//                   useAnimation
+//                     ? useMove
+//                       ? styles.moveLine
+//                       : appearType === "draw"
+//                       ? styles.drawLine
+//                       : appearType === "fade"
+//                       ? styles.fadeLine
+//                       : ""
+//                     : ""
+//                 }
+//                 style={{
+//                   "--line-width": `${height}px`,
+//                   "--line-offset": `${
+//                     (!horizontal && animationYAxisStart === "bottom") || (horizontal && animationYAxisStart !== "bottom") ? -height : height
+//                   }px`,
+//                   "--animation-duration": `${useMove ? moveDuration : appearDuration}s`,
+//                   "--animation-timing-function": useMove ? moveTimingFunction : appearTimingFunction,
+//                   "--animation-delay": `${
+//                     (useMove ? moveStartDelay : appearStartDelay) +
+//                     (useMove ? moveItemDelay : appearItemDelay) * (animationXAxisStart === "left" ? idx : xAxis.length - 1 - idx) +
+//                     (showEndLine && !useMove ? appearItemDelay : 0)
+//                   }s`,
+//                   "--width-offset": horizontal ? `0px,${move}px` : `${move}px`
+//                 }}
+//               ></line>
+//             );
+//           })}
+//         </g>
+//       }
+//       {showEndLine && (
+//         <g transform={horizontal ? `translate(0,${width})` : `translate(${width},0)`}>
+//           <line
+//             key={"background-line-x-" + 0 + "-" + 0}
+//             x1={horizontal ? "0" : 0}
+//             x2={horizontal ? height : 0}
+//             y1={horizontal ? 0 : "0"}
+//             y2={horizontal ? 0 : height}
+//             stroke={lineColor}
+//             strokeOpacity={lineOpacity}
+//             strokeWidth={lineWidth}
+//             strokeDasharray={lineDash ? `${lineDashWidth},${lineDashGap}` : 0}
+//             strokeLinecap={lineRound ? "round" : ""}
+//             className={useAnimation ? (appearType === "draw" ? styles.drawLine : appearType === "fade" ? styles.fadeLine : "") : ""}
+//             style={{
+//               "--line-width": `${height}px`,
+//               "--line-offset": `${
+//                 (!horizontal && animationYAxisStart === "bottom") || (horizontal && animationYAxisStart !== "bottom") ? -height : height
+//               }px`,
+//               "--animation-duration": `${appearDuration}s`,
+//               "--animation-timing-function": "ease",
+//               "--animation-delay": `${appearStartDelay + appearItemDelay * (animationXAxisStart === "left" ? xAxis.length + 1 : 0)}s`
+//             }}
+//           ></line>
+//         </g>
+//       )}
+//     </g>
+//   );
+// };
+// /* eslint-enable complexity */
+
 import { useRef } from "react";
 
 import styles from "./x-axis-grid-line.module.css";
@@ -89,17 +253,44 @@ export const DrawXAxisGridLine = ({
               }
             }
 
+            let linePath = "";
+
+            if (lineDash) {
+              let path = horizontal ? `M 0,${x - move} ` : `M ${x - move},0 `;
+              let pathLength = 0;
+
+              while (pathLength < height) {
+                if (pathLength + lineDashWidth + lineDashGap <= height) {
+                  path += horizontal ? `h ${lineDashWidth} m ${lineDashGap} 0 ` : `v ${lineDashWidth} m 0 ${lineDashGap} `;
+                } else if (pathLength + lineDashWidth <= height) {
+                  path += horizontal ? `h ${lineDashWidth}` : `v ${lineDashWidth}`;
+                } else {
+                  path += horizontal ? `h ${height - pathLength}` : `v ${height - pathLength}`;
+                }
+
+                pathLength += lineDashWidth + lineDashGap;
+              }
+
+              linePath = path;
+            } else {
+              linePath = horizontal
+                ? `
+                M ${x - move},0
+                h ${height}
+              `
+                : `
+                M 0,${x - move}
+                v ${height}
+              `;
+            }
+
             return (
-              <line
+              <path
                 key={"background-line-x-" + ms + "-" + d}
-                x1={horizontal ? "0" : x - move}
-                x2={horizontal ? height : x - move}
-                y1={horizontal ? x - move : "0"}
-                y2={horizontal ? x - move : height}
+                d={linePath}
                 stroke={lineColor}
                 strokeOpacity={lineOpacity}
                 strokeWidth={lineWidth}
-                strokeDasharray={lineDash && d !== 0 ? `${lineDashWidth},${lineDashGap}` : "0"}
                 strokeLinecap={lineRound ? "round" : ""}
                 className={
                   useAnimation
@@ -115,7 +306,11 @@ export const DrawXAxisGridLine = ({
                 style={{
                   "--line-width": `${height}px`,
                   "--line-offset": `${
-                    (!horizontal && animationYAxisStart === "bottom") || (horizontal && animationYAxisStart !== "bottom") ? -height : height
+                    lineDash
+                      ? -lineDashWidth
+                      : (!horizontal && animationYAxisStart === "bottom") || (horizontal && animationYAxisStart !== "bottom")
+                      ? -height
+                      : height
                   }px`,
                   "--animation-duration": `${useMove ? moveDuration : appearDuration}s`,
                   "--animation-timing-function": useMove ? moveTimingFunction : appearTimingFunction,
@@ -126,7 +321,7 @@ export const DrawXAxisGridLine = ({
                   }s`,
                   "--width-offset": horizontal ? `0px,${move}px` : `${move}px`
                 }}
-              ></line>
+              ></path>
             );
           })}
         </g>
--- src/jetty-chart/src/components/y-axis/draw-y-axis-grid-line.js
+++ src/jetty-chart/src/components/y-axis/draw-y-axis-grid-line.js
@@ -1,105 +1,3 @@
-// import { useRef } from "react";
-
-// import styles from "./y-axis-grid-line.module.css";
-
-// /* eslint-disable complexity */
-// export const DrawYAxisGridLine = ({
-//   normalSettings: { horizontal, yAxis, width, yAxisHeight, showTopScope },
-//   lineSettings: { lineVisible, lineOpacity, lineColor, lineWidth, lineDash, lineDashWidth, lineDashGap, lineRound },
-//   animationSettings: {
-//     useAnimation,
-//     appearType,
-//     appearDuration,
-//     appearStartDelay,
-//     appearItemDelay,
-//     appearTimingFunction,
-//     appearStartFrom,
-//     moveLine,
-//     moveDuration,
-//     moveStartDelay,
-//     moveItemDelay,
-//     moveTimingFunction
-//   }
-// }) => {
-//   const prevYAxis = useRef({});
-//   const prevYAxisTemp = useRef({});
-
-//   if (!lineVisible) {
-//     return;
-//   }
-
-//   const animationXAxisStart = appearStartFrom.split("-")[0];
-//   const animationYAxisStart = appearStartFrom.split("-")[1];
-//   const prevYAxisKeys = Object.keys(prevYAxis.current);
-//   const ms = new Date().valueOf();
-
-//   if (moveLine) {
-//     prevYAxis.current = { ...prevYAxisTemp.current };
-//     prevYAxisTemp.current = [];
-//   }
-
-//   return (
-//     <g>
-//       {yAxis.map((c, idx) => {
-//         if (!showTopScope && (idx === 0 || idx === yAxis.length - 1) && c !== 0) {
-//           return;
-//         }
-
-//         // 현재 위치 계산
-//         const location = yAxisHeight * idx;
-
-//         // 현재 위치 정보 저장
-//         prevYAxisTemp.current[c] = location;
-
-//         // 라인 리렌더링을 안할 경우
-//         let useMove = false;
-//         let move = 0;
-
-//         if (moveLine) {
-//           // 이전 위치에 현재 위치가 포함되는지 확인
-//           if (prevYAxisKeys.includes(String(c))) {
-//             move = location - prevYAxis.current[c];
-//             useMove = true;
-//           }
-//         }
-
-//         return (
-//           <line
-//             key={"background-line-y-" + ms + "-" + c}
-//             x1={horizontal ? location - move : "0"}
-//             x2={horizontal ? location - move : width}
-//             y1={horizontal ? "0" : location - move}
-//             y2={horizontal ? width : location - move}
-//             stroke={lineColor}
-//             strokeOpacity={lineOpacity}
-//             strokeWidth={lineWidth}
-//             strokeDasharray={lineDash && c !== 0 ? `${lineDashWidth},${lineDashGap}` : "0"}
-//             strokeLinecap={lineRound ? "round" : ""}
-//             className={
-//               useAnimation ? (useMove ? styles.moveLine : appearType === "draw" ? styles.drawLine : appearType === "fade" ? styles.fadeLine : "") : ""
-//             }
-//             style={{
-//               "--line-width": `${width}px`,
-//               "--line-offset": `${animationXAxisStart === "left" ? width : -width}px`,
-//               "--animation-duration": `${useMove ? moveDuration : appearDuration}s`,
-//               "--animation-timing-function": useMove ? moveTimingFunction : appearTimingFunction,
-//               "--animation-delay": `${
-//                 (useMove ? moveStartDelay : appearStartDelay) +
-//                 (useMove ? moveItemDelay : appearItemDelay) *
-//                   ((!horizontal && animationYAxisStart === "bottom") || (horizontal && animationYAxisStart !== "bottom")
-//                     ? yAxis.length - 1 - idx
-//                     : idx)
-//               }s`,
-//               "--height-offset": horizontal ? `${move}px` : `0px,${move}px`
-//             }}
-//           ></line>
-//         );
-//       })}
-//     </g>
-//   );
-// };
-// /* eslint-enable complexity */
-
 import { useRef } from "react";
 
 import styles from "./y-axis-grid-line.module.css";
@@ -174,6 +72,10 @@ export const DrawYAxisGridLine = ({
           while (pathLength < width) {
             if (pathLength + lineDashWidth + lineDashGap <= width) {
               path += horizontal ? `v ${lineDashWidth} m 0 ${lineDashGap} ` : `h ${lineDashWidth} m ${lineDashGap} 0 `;
+            } else if (pathLength + lineDashWidth <= width) {
+              path += horizontal ? `v ${lineDashWidth}` : `h ${lineDashWidth}`;
+            } else {
+              path += horizontal ? `v ${width - pathLength}` : `h ${width - pathLength}`;
             }
 
             pathLength += lineDashWidth + lineDashGap;
@@ -199,12 +101,13 @@ export const DrawYAxisGridLine = ({
             stroke={lineColor}
             strokeOpacity={lineOpacity}
             strokeWidth={lineWidth}
+            strokeLinecap={lineRound ? "round" : ""}
             className={
               useAnimation ? (useMove ? styles.moveLine : appearType === "draw" ? styles.drawLine : appearType === "fade" ? styles.fadeLine : "") : ""
             }
             style={{
               "--line-width": `${width}px`,
-              "--line-offset": `${animationXAxisStart === "left" ? width : -width}px`,
+              "--line-offset": `${lineDash ? -lineDashWidth : animationXAxisStart === "left" ? width : -width}px`,
               "--animation-duration": `${useMove ? moveDuration : appearDuration}s`,
               "--animation-timing-function": useMove ? moveTimingFunction : appearTimingFunction,
               "--animation-delay": `${
