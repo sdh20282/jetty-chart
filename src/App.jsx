c2be0dfd63233321e3f7d98d32400ca28559835d
sdh20282 <sdh20282@gmail.com>
2023-10-31 16:51:59 +0900

    Refactor : bar grow animation 리팩토링 - path -> rect 태그로 변경 - text animation 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { NormalBar } from "./jetty-chart/src";
 
-const counts = [5, 6, 6, 7, 8];
+const counts = [5, 6, 6];
 
 const myNormalBar = ({ data }) => {
   return <NormalBar data={data} keys={["target1"]} xLegend={"types"} yLegend={"values"} />;
@@ -17,7 +17,7 @@ function App() {
 
     for (let index = 0; index < counts[Math.floor(Math.random() * counts.length)]; index++) {
       arr.push({
-        value: Math.floor(Math.random() * 100),
+        value: Math.floor(Math.random() * 90) + 10,
         label: "test" + (index + 1)
       });
     }
@@ -28,7 +28,7 @@ function App() {
     });
 
     arr.push({
-      value: -2,
+      value: -20,
       label: "asdasdff"
     });
 
