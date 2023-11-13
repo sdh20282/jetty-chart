7b83cf0f0f71d31ebed59a051b700460b88ae201
J3SUNG <wptjd6141@naver.com>
2023-11-13 17:18:32 +0900

    Fix : 파이 차트 conflict 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,5 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { Pie } from "./jetty-chart/src";
-// import { NormalBar } from "./jetty-chart/src";
-
-const counts = [3, 4, 5, 6, 7];
-
-// const myNormalBar = ({ data }) => {
-//   return <NormalBar data={data} keys={["target1"]} xLegend={"types"} yLegend={"values"} />;
-// };
-
 function App() {
-  const [data, setData] = useState([
-    { value: 10, label: "test1" },
-    { value: 20, label: "test2" },
-    { value: 30, label: "test3" },
-  ]);
-  const [state, setState] = useState(1);
-
-  useEffect(() => {
-    const arr = [];
-
-    for (let index = 0; index < counts[Math.floor(Math.random() * counts.length)]; index++) {
-      arr.push({
-        value: Math.floor(Math.random() * 90) + 10,
-        label: "test" + (index + 1),
-      });
-    }
-
-    setData(arr);
-  }, [state]);
-
-  return (
-    <div>
-      {/* {myNormalBar({ data })} */}
-      <Pie data={data} />
-      <button
-        onClick={() => {
-          setState((state) => 1 - state);
-        }}
-      >
-        갱신
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/jetty-chart/src/index.js
+++
@@ -1,4 +0,0 @@
-import { NormalBar } from "./bars/bars";
-import { Pie } from "./pies/pies";
-
-export { NormalBar, Pie };
