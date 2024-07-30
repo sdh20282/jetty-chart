7fe346512197aeeb233b8bbffcf11564a49915a2
sdh20282 <sdh20282@gmail.com>
2024-07-31 06:31:02 +0900

    refactor: add typescript on project

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { Pie } from "./jetty-chart/src";
-
-function App() {
-  return (
-    <div style={{ marginTop: "50px", paddingLeft: "200px" }}></div>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div>Hello!</div>
+  );
+}
+
+export default App;
